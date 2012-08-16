f6fe28a635b8cdcee3bad767cced4d1415999c78
Kori Roys <kori@koriroys.com>
2012-08-15 19:06:48 -0500

    login and logout works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,10 @@ class SessionsController < ApplicationController
     flash[:notice] = 'Logged in Successfully'
     redirect_to root_path
   end
+
+  def destroy
+    reset_session
+    flash[:notice] = "Logged out Successfully"
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ GithubAuth::Application.routes.draw do
   root to: 'static_pages#index'
   get '/auth/github', as: :github_auth
   match '/auth/github/callback' => 'sessions#create'
+  get '/logout' => 'sessions#destroy', as: :logout
 end
